c52a6cad88c26ec26d97e711683b2cee0eb5259b
Samo <samo.cibej@dijak.stps-tr.si>
2024-10-12 19:34:08 +0200

    gridgen start

--- packages/grid_gen.py
+++ packages/grid_gen.py
@@ -1,2 +1,21 @@
+import numpy as np
 from random import randint
 
+
+def grid_gen(row=randint(4, 40), col=randint(4, 40)) -> np.ndarray:
+    grid = np.stack([[randint(0, 1) for _ in range(col)] for _ in range(row)])
+    return grid
+
+
+def point_gen(grid:np.ndarray) -> ((int, int), (int, int)):
+    shape = grid.shape
+    row = shape[0]
+    col = shape[1]
+
+    point_a = (randint(0, row-1), randint(0, col-1))
+    point_b =(randint(0, row-1), randint(0, col-1))
+
+    while point_a != point_b:
+        point_b = (randint(0, row - 1), randint(0, col - 1))
+
+    return point_a, point_b
