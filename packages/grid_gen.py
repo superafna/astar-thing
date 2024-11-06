0fa8d97daad30926d410da8009c1000d01c366c6
Samo <samo.cibej@dijak.stps-tr.si>
2024-11-06 20:33:54 +0100

    manjsi slucajni popravek.

--- packages/grid_gen.py
+++ packages/grid_gen.py
@@ -2,7 +2,7 @@ import numpy as np
 from random import randint
 
 
-def grid_gen(row=randint(4, 40), col=randint(4, 40)) -> np.ndarray:
+def grid_gen(row=randint(50, 100), col=randint(50, 100)) -> np.ndarray:
     """
     Generate a two-dimensional numpy array of the specified size, filled randomly with 1's and 0's.
     Parameters are random, if not specified.
@@ -10,7 +10,7 @@ def grid_gen(row=randint(4, 40), col=randint(4, 40)) -> np.ndarray:
     :param col: number of columns in grid.
     :return: numpy ndarray of grid.
     """
-    grid = np.stack([[max(randint(0, 1), randint(0, 1), randint(0, 1)) for _ in range(col)] for _ in range(row)])
+    grid = np.stack([[max(randint(0, 1), randint(0, 1)) for _ in range(col)] for _ in range(row)])
     return grid
 
 
@@ -23,5 +23,16 @@ def point_gen(grid:np.ndarray) -> tuple[int, int]:
     shape = grid.shape
     row = shape[0]
     col = shape[1]
+
+    return randint(0, row-1), randint(0, col-1)
+
+
+def point_gen_new(row:int, col:int) -> tuple[int, int]:
+    """
+    Randomly generate a point within a 2d grid.
+    :param row: len of row.
+    :param col: len of grid.
+    :return: coordinates as tuple of ints.
+    """
 
     return randint(0, row-1), randint(0, col-1)
